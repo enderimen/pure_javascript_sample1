8078adce1407ec811cb4f585d28c15339e04580c
Ender İMEN <enderimen@hotmail.com>
2018-02-13 21:21:45 +0300

    Sounds added.

---
+++ js/customize.js
@@ -0,0 +1,16 @@
+window.addEventListener('keydown',function (e) {
+	
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	if (!audio) return;
+
+	audio.currentTime = 0;
+	audio.play();
+
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+	key.classList.add("playing");
+
+	setTimeout(function () {
+		key.classList.remove("playing");
+	},100);
+});
